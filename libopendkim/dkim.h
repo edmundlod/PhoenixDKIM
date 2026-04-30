995c815ac90612fc47a647709482ec9cf2884fdb
Edmund Lodewijks <edmund@proteamail.com>
2026-04-30 10:03:38 +0200

    Remove ATPS and base32 library files - daemon cleanup deferred to daemon session

--- libopendkim/dkim.h
+++ libopendkim/dkim.h
@@ -1824,27 +1824,6 @@ extern const char *dkim_qi_getname __P((DKIM_QUERYINFO *));
 extern int dkim_qi_gettype __P((DKIM_QUERYINFO *));
 
 /*
-**  DKIM_BASE32_ENCODE -- encode a string using base32
-**
-**  Parameters:
-**  	buf -- destination buffer
-**  	buflen -- bytes available at buf (updated)
-**  	data -- pointer to data to encode
-**  	size -- bytes at "data" to encode
-**
-**  Return value:
-**  	Length of encoding.
-**
-**  Notes:
-**  	buf should be at least a byte more than *buflen to hold the trailing
-**  	'\0'.
-**
-**  	*buflen is updated to count the number of bytes read from "data".
-*/
-
-extern int dkim_base32_encode __P((char *, size_t *, const void *, size_t));
-
-/*
 **  DKIM_SIG_GETHASHES -- retrieve hashes
 **
 **  Parameters:
